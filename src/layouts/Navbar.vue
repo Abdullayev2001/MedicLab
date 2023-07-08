c97596eead296bcff91466ba649e5e358b483fb3
Nurillo <abdullayevnurilloh30@gmail.com>
2023-07-08 19:15:40 +0500

    navbar menu

--- src/layouts/Navbar.vue
+++ src/layouts/Navbar.vue
@@ -7,21 +7,32 @@
                         <h1>Medical Lab</h1>
                     </span>
                 </div>
-                <ul class="menu">
+                <ul class="menu" v-if="!is_mobile">
                     <li v-for="item in menu_array" :key="item.text">
-                        <router-link to="">{{item.text}}</router-link>
+                        <router-link class="link" to="">{{item.text}}</router-link>
                     </li>
                 </ul>
-                <div class="burger"></div>
-                <div class="mobile_menu"></div>
+                <div 
+                 class="burger"
+                 v-if="is_mobile"
+                 @click="is_burger_open = !is_burger_open">
+                    <BurgerMenu  :is_active="is_burger_open"/>
+                </div>
+                <div class="mobile_menu" v-if="is_mobile"></div>
                 <div class="actions">
                     <div class="search">
-                        <i class="fas fa-search"></i>
+                        <i class="fas fa-search" v-if="!is_mobile"></i>
                     </div>
-                </div>
-                <div class="language">
-                    <span>ENG</span>
-                    <i class="fas fa-angle-down"></i>
+                    <div class="language" @click="toggleDropdown">
+                        <span>ENG</span>
+                        <i  class="fas fa-angle-down"  :style="{transform: is_dropdown_open ?`rotate(180deg)` : `rotate(0)`,
+                        transition:'0.3s'
+                        }"></i>
+                        <ul class="dropdown " @click.stop :class="{active: is_dropdown_open}" >
+                            <li>rus</li>
+                            <li>uzb</li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </div>
@@ -30,11 +41,18 @@
 
 <script setup>
 import menu from '../frags/menu.js'
+import BurgerMenu from './BurgerMenu.vue'
 import { ref } from 'vue'
 
 const menu_array = menu
+const is_dropdown_open = ref(false)
+const is_burger_open = ref(false)
+const is_mobile = ref(true)
+const toggleDropdown = ()=>{
+    is_dropdown_open.value = !is_dropdown_open.value
+}
 </script>
 
 <style lang="scss">
-
+  @import '@/layouts/styles/_navbar.scss';
 </style>
